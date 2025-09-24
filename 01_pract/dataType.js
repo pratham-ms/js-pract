2ea32c0da0f75fb2feaf3757bed3eea9e39ced33
pratham-ms <pratham@monkshadow.com>
2025-09-24 07:15:38 +0000

    dataTypes

---
+++ 01_pract/dataType.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+
+let age = 20
+let isLoggedIn = false
+let firstName = "Pratham"
+let lastName = null
+let middleName = undefined
+
+/*
+number - 2 to the power 53
+bigint - larger than 2 to the power 53
+string - ""
+boolean - true/false
+null - standalone value
+undefined - variable not assigned a value
+object - key value pair
+symbol - unique value
+*/
+
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+console.log(typeof age); //number
+console.log(typeof isLoggedIn); //boolean
+console.log(typeof firstName); //string
--- 01_pract/test.js
+++ 01_pract/test.js
@@ -1,1 +1,9 @@
-console.log("Pratham");
+const accountId = 230905
+let accountEmail = "pratham@monshadow.com"
+var accountPassword = "12345"
+accountCity = "Surat"
+
+//accountId = 3456
+
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword, accountCity])

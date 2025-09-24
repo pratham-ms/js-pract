02edf740b08d8fc0763a656374c6c2dfb794ba60
pratham-ms <pratham@monkshadow.com>
2025-09-24 07:29:11 +0000

    conversion og datatypes

---
+++ 01_pract/con_operations.js
@@ -0,0 +1,22 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true ; 0=> false
+// "" => false
+
+let someNumber = 123
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
